ce6ae1ca026e63ed7ba5db71d9aaf839c1d22037
Cristina-Pat <calota.cristina@gmail.com>
2024-01-17 20:34:04 +0000

    Adjusted colours for passed and failed test statuses

--- spec/test.framework.js
+++ spec/test.framework.js
@@ -1,16 +1,16 @@
 
 export const assertEquals = (actualOutput, expectedOutput, testName) => {
     if (actualOutput !== expectedOutput) {
-        console.log(`${testName} fails - expected ${expectedOutput}, but got ${actualOutput}`);
+        console.log("\x1b[31m%s\x1b[0m", `\t${testName} fails - expected false, but got ${actualOutput}`);
     } else {
-        console.log(`${testName} passes`);
+        console.log("\x1b[32m%s\x1b[0m", `\t${testName} passes`);
     }
 }
 
 export const assertFalse = (actualOutput, testName) => {
     if (actualOutput !== false){
-        console.log(`${testName} fails - expected false, but got ${actualOutput}`);
+        console.log("\x1b[31m%s\x1b[0m", `\t${testName} fails - expected false, but got ${actualOutput}`);
     }else {
-        console.log(`${testName} passes`);
+        console.log("\x1b[32m%s\x1b[0m", `\t${testName} passes`);
     }
 }
